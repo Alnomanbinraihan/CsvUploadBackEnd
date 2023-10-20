cdcc9a1a0dfede564ed2478e9f08eca6986a8fc6
Alnomanbinraihan <alnoman015717@gmail.com>
2023-10-20 09:32:53 +0600

    @CrossOrigin added

--- src/main/java/com/example/uploadCsv/controller/CsvUploadController.java
+++ src/main/java/com/example/uploadCsv/controller/CsvUploadController.java
@@ -4,12 +4,14 @@ import com.example.uploadCsv.service.CsvUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@CrossOrigin("*")
 public class CsvUploadController {
 
     @Autowired
